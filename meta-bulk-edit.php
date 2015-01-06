880d7ef41bcdbf612608de15e6d9cb7ed119999b
Michael Jordan <chiangmaiguy@gmail.com>
2015-01-06 09:57:01 -0500

    Add validation functions

--- meta-bulk-edit.php
+++ meta-bulk-edit.php
@@ -22,8 +22,8 @@ class Storm_Meta_Bulk_Edit {
 	 * @var array Configuration
 	 */
 	private $meta_settings = array(
-		'title' => 'Meta Name', // human-readable name
-		'slug' => 'meta-name', // meta_key as in the wp_postmeta table
+		'title' => 'Email', // human-readable name
+		'slug' => 'email', // meta_key as in the wp_postmeta table
 		'input_type' => 'input', // string input type
 		'post_type' => 'post',
 		'empty_message' => 'None' // default text when no data found
@@ -53,8 +53,8 @@ class Storm_Meta_Bulk_Edit {
 	 */
 	protected function init() {
 		add_action( 'admin_print_scripts-edit.php', array( $this, 'enqueue_edit_scripts' ) );		
-		add_filter( 'manage_'.$this->meta_settings['post_type'].'_posts_columns', array( $this, 'edit_meta_columns' ), 1 );
-		add_action( 'manage_'.$this->meta_settings['post_type'].'_posts_custom_column', array( $this, 'manage_meta_columns' ), 10, 2 );
+		add_filter( 'manage_'.esc_attr( $this->meta_settings['post_type'] ).'_posts_columns', array( $this, 'edit_meta_columns' ), 1 );
+		add_action( 'manage_'.esc_attr( $this->meta_settings['post_type'] ).'_posts_custom_column', array( $this, 'manage_meta_columns' ), 10, 2 );
 		add_action( 'bulk_edit_custom_box', array( $this, 'add_to_bulk_quick_edit_custom_box' ), 10, 2 );
 		add_action( 'quick_edit_custom_box', array( $this, 'add_to_bulk_quick_edit_custom_box' ), 10, 2 );
 		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
@@ -73,7 +73,7 @@ class Storm_Meta_Bulk_Edit {
 	 */
 	function edit_meta_columns( $columns ) {
 		
-		$columns[$this->meta_settings['slug']] = $this->meta_settings['title'];
+		$columns[$this->meta_settings['slug']] = esc_html( $this->meta_settings['title'] );
 		
 		return $columns;
 	}
@@ -90,13 +90,13 @@ class Storm_Meta_Bulk_Edit {
 			case $this->meta_settings['slug'] :
 			
 				/* Get the post meta. */
-				$custom_meta = get_post_meta( $post_id, $this->meta_settings['slug'], true );
+				$custom_meta = get_post_meta( $post_id, esc_html( $this->meta_settings['slug'] ), true );
 				
 				/* If no custom meta is found, output a default message. */
 				if ( empty( $custom_meta ) )
 					echo $this->meta_settings['empty_message'];
 				else
-					echo '<div class="storm-meta-input" id="storm-meta-input-' . $post_id . '">' . get_post_meta( $post_id, $this->meta_settings['slug'], true ) . '</div>';
+					echo '<div class="storm-meta-input" id="storm-meta-input-' . absint( $post_id ) . '">' . get_post_meta( $post_id, esc_html( $this->meta_settings['slug'] ), true ) . '</div>';
 				break;
 				
 			default :
@@ -116,8 +116,8 @@ class Storm_Meta_Bulk_Edit {
 	               ?><fieldset class="inline-edit-col-right">
 	                  <div class="inline-edit-group">
 	                     <label>
-	                        <span class="title"><?php echo $this->meta_settings['title']; ?></span>
-	                        <input type="<?php echo $this->meta_settings['input_type']; ?> " name="storm_meta_input" value="" />
+	                        <span class="title"><?php echo esc_html( $this->meta_settings['title'] ); ?></span>
+	                        <input type="<?php echo esc_attr( $this->meta_settings['input_type'] ); ?> " name="storm_meta_input" value="" />
 	                     </label>
 	                  </div>
 	               </fieldset><?php
@@ -145,7 +145,7 @@ class Storm_Meta_Bulk_Edit {
 			case $this->meta_settings['post_type']:
 	
 			if ( array_key_exists( 'storm_meta_input', $_POST ) )
-				update_post_meta( $post_id, $this->meta_settings['slug'], $_POST[ 'storm_meta_input' ] );
+				update_post_meta( $post_id, esc_html( $this->meta_settings['slug'] ), sanitize_text_field( $_POST[ 'storm_meta_input' ] ) );
 			
 		 	break;
 	   }
@@ -161,7 +161,7 @@ class Storm_Meta_Bulk_Edit {
 		// if everything is in order
 		if ( !empty( $post_ids ) && is_array( $post_ids ) && !empty( $meta_value ) ) {
 			foreach( $post_ids as $post_id ) {
-				update_post_meta( $post_id, $this->meta_settings['slug'], $meta_value );
+				update_post_meta( $post_id, esc_html( $this->meta_settings['slug'] ), sanitize_text_field( $meta_value ) );
 			}
 		}
 	}
